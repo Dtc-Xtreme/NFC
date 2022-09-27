e8d3aff45d82f66ac900ffc939a757cd583a2c5c
Dtc-Xtreme <dtc-xtreme@hotmail.com>
2022-09-27 12:13:33 +0200

    Languages (EN, NL) added in settings

--- MobileDev/ViewModels/SettingsViewModel.cs
+++ MobileDev/ViewModels/SettingsViewModel.cs
@@ -1,8 +1,11 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using banditoth.MAUI.Multilanguage.Interfaces;
+using CommunityToolkit.Mvvm.ComponentModel;
+using IntelliJ.Lang.Annotations;
 using Microsoft.Extensions.Configuration;
 using Plugin.NFC;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +15,16 @@ namespace MobileDev.ViewModels
     public partial class SettingsViewModel : BaseViewModel
     {
         private IConfiguration configuration;
+        private ITranslator translator;
         private bool darkTheme = false;
+        private string language;
         private bool nfcIsEnabled;
 
-        public SettingsViewModel(IConfiguration conf)
+        public SettingsViewModel(IConfiguration conf, ITranslator trans)
         {
             this.configuration = conf;
+            this.translator = trans;
+            Language = this.translator.CurrentCulture.IetfLanguageTag;
             var deviceTheme = Application.Current.PlatformAppTheme;
             if (Application.Current.UserAppTheme == AppTheme.Unspecified)
             {
@@ -49,6 +56,16 @@ namespace MobileDev.ViewModels
                     Application.Current.UserAppTheme = AppTheme.Light;
                 }
                 OnPropertyChanged();
+            }
+        }
+        public string Language
+        {
+            get => language;
+            set
+            {
+                language = value;
+                OnPropertyChanged();
+                translator.SetCurrentCulture(new CultureInfo(value));
             }
         }
         public bool NfcIsEnabled
