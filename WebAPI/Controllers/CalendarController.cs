f042b9d9f4fa2a8288ab6ae14a108a19d4ab933d
Dtc-Xtreme <dtc-xtreme@hotmail.com>
2022-09-30 00:30:59 +0200

    Wep API for calendar items

---
+++ WebAPI/Controllers/CalendarController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Globalization;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("Calendar")]
+    public class CalendarController : Controller
+    {
+        private static readonly CalendarItem[] CalenderItems = new[]
+        {
+            new CalendarItem(DateTime.Parse("01/10/2022"), "Competitie", "Boekt A VS Boekt B"),
+            new CalendarItem(DateTime.Parse("08/10/2022"), "Competitie", "Boekt A VS Kelchteren A"),
+            new CalendarItem(DateTime.Parse("15/10/2022"), "Competitie", "Boekt A VS Genenbos B"),
+            new CalendarItem(DateTime.Parse("22/10/2022"), "Competitie", "Boekt A VS Sparrendaal B"),
+            new CalendarItem(DateTime.Parse("29/10/2022"), "Competitie", "Boekt A VS Maasmechelen B"),
+        };
+
+        [HttpGet(Name = "GetCalendar")]
+        public IEnumerable<CalendarItem> Get()
+        {
+            return CalenderItems.ToList();
+        }
+
+    }
+}
---
+++ WebAPI/Models/CalendarItem.cs
@@ -0,0 +1,24 @@
+﻿namespace WebAPI.Models
+{
+    public class CalendarItem
+    {
+        private DateTime date;
+        private string title;
+        private string description;
+
+        public CalendarItem()
+        {
+
+        }
+        public CalendarItem(DateTime date, string title, string description)
+        {
+            this.date = date;
+            this.title = title;
+            this.description = description;
+        }
+
+        public DateTime Date { get => date; set => date = value; }
+        public string Title { get => title; set => title = value; }
+        public string Description { get => description; set => description = value; }
+    }
+}
