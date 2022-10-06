1464ae8a5dcbbf3412c7da632337e5bf918dd528
dtc-xtreme <dtc-xtreme@hotmail.com>
2022-10-07 00:23:51 +0200

    proxy redirect on apache -> disabled https

--- WebAPI/Controllers/CalendarController.cs
+++ WebAPI/Controllers/CalendarController.cs
@@ -9,13 +9,16 @@ namespace WebAPI.Controllers
     [Route("Calendar")]
     public class CalendarController : Controller
     {
+        private static CultureInfo provider = new CultureInfo("nl-BE");
+        private static string format = "dd/mm/yyyy";
+
         private static readonly CalendarItem[] CalenderItems = new[]
         {
-            new CalendarItem(DateTime.Parse("01/10/2022"), "Competitie", "Boekt A VS Boekt B"),
-            new CalendarItem(DateTime.Parse("08/10/2022"), "Competitie", "Boekt A VS Kelchteren A"),
-            new CalendarItem(DateTime.Parse("15/10/2022"), "Competitie", "Boekt A VS Genenbos B"),
-            new CalendarItem(DateTime.Parse("22/10/2022"), "Competitie", "Boekt A VS Sparrendaal B"),
-            new CalendarItem(DateTime.Parse("29/10/2022"), "Competitie", "Boekt A VS Maasmechelen B"),
+            new CalendarItem(DateTime.ParseExact("01/10/2022", format, provider), "Competitie", "Boekt A VS Boekt B"),
+            new CalendarItem(DateTime.ParseExact("08/10/2022", format, provider), "Competitie", "Boekt A VS Kelchteren A"),
+            new CalendarItem(DateTime.ParseExact("15/10/2022", format, provider), "Competitie", "Boekt A VS Genenbos B"),
+            new CalendarItem(DateTime.ParseExact("22/10/2022", format, provider), "Competitie", "Boekt A VS Sparrendaal B"),
+            new CalendarItem(DateTime.ParseExact("29/10/2022", format, provider), "Competitie", "Boekt A VS Maasmechelen B"),
         };
 
         [HttpGet]
