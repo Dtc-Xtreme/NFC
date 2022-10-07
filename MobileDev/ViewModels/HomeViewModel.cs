37565dda17022474af2f932b3440578e410c5ce5
dtc-xtreme <dtc-xtreme@hotmail.com>
2022-10-07 14:24:47 +0200

    add certificate authority to use api

--- MobileDev/ViewModels/HomeViewModel.cs
+++ MobileDev/ViewModels/HomeViewModel.cs
@@ -42,28 +42,17 @@ namespace MobileDev.ViewModels
         [RelayCommand]
         private async void GetAPI()
         {
-            //HttpClientHandler handler = new HttpClientHandler();
-            //handler.ServerCertificateCustomValidationCallback = ServerCertificateCustomValidation;
-            HttpClient client = new HttpClient();
-            ////HttpResponseMessage response = await client.GetAsync("http://10.0.10.6:7163/Calendar");
-            HttpResponseMessage response = await client.GetAsync("https://home.dtc-xtreme.net:7163/Calendar");
-            response.EnsureSuccessStatusCode();
-            string responseBody = await response.Content.ReadAsStringAsync();
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync("https://home.dtc-xtreme.net:7163/Calendar");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+            }catch(Exception ex)
+            {
+                //
+                Console.WriteLine("Errors: " + ex.Message);
+            }
         }
-
-        //private static bool ServerCertificateCustomValidation(HttpRequestMessage requestMessage, X509Certificate2 certificate, X509Chain chain, SslPolicyErrors sslErrors)
-        //{
-        //    // It is possible to inspect the certificate provided by the server.
-        //    Console.WriteLine($"Requested URI: {requestMessage.RequestUri}");
-        //    Console.WriteLine($"Effective date: {certificate.GetEffectiveDateString()}");
-        //    Console.WriteLine($"Exp date: {certificate.GetExpirationDateString()}");
-        //    Console.WriteLine($"Issuer: {certificate.Issuer}");
-        //    Console.WriteLine($"Subject: {certificate.Subject}");
-
-        //    // Based on the custom logic it is possible to decide whether the client considers certificate valid or not
-        //    Console.WriteLine($"Errors: {sslErrors}");
-        //    return sslErrors == SslPolicyErrors.None;
-        //}
-
     }
 }
